6b77cd28cfd5880930ead12692f894395f6d48a5
Nikolas Hamon <54676930+nhamon321@users.noreply.github.com>
2020-02-20 22:12:02 -0600

    Add files via upload

---
+++ NH20_A1EX1.cpp
@@ -0,0 +1,14 @@
+// Grade Book #20
+// Program that displays my first name
+// and last name in a box
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "*---------------*\n";
+    cout << "| Nikolas Hamon |" << endl;
+    cout << "*---------------*" << endl;
+    return 0;
+}
